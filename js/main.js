6a861e13d046c2b457d055b73c2b77a7e6b0d8c9
timwis <tim@timwis.com>
2015-03-28 23:23:17 -0400

    Details page

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ _.templateSettings.variable = 'data';
 var layout = new Marionette.LayoutView({
      el: 'body',
      regions: {
-          'main': '#main'
+          'top-leaders': '#top-leaders',
+          'details': '#details'
      }
 });
 
@@ -28,7 +29,19 @@ var WardLeader = Backbone.Model.extend({
           // Set default photo if no photo provided
           this.set('avatar', this.get('Photo') ? this.get('Photo') : this.get('Gender') === 'F' ? 'img/avatar-female.png' : 'img/avatar-male.png');
      }
-})
+});
+
+var comparators = {
+     voters: function(item) {
+          return -item.get('Total Registered');
+     },
+     turnout: function(item) {
+          return -item.get('Turnout 2014 General');
+     },
+     vacancies: function(item) {
+          return -item.get('vacancies');
+     }
+};
 
 var WardLeaders = Backbone.Collection.extend({
      model: WardLeader,
@@ -37,32 +50,64 @@ var WardLeaders = Backbone.Collection.extend({
           sheet: 'Democratic'
      },
      sync: Backbone.tabletopSync,
-     comparator: function(item) {
-          return -item.get('Total Registered');
-     }
+     comparator: comparators.voters
 });
 
 var TopLeadersItemView = Backbone.Marionette.ItemView.extend({
-     template: '#tmpl-top-leaders-item'
+     template: '#tmpl-top-leaders-item',
+     initialize: function() {
+          _.bindAll(this, 'onDetails');
+     },
+     events: {
+          'click [data-ward]': 'onDetails'
+     },
+     onDetails: function(e) {
+          console.log(this.model);
+          var wardLeaderView = new WardLeaderView({ model: this.model });
+          layout.getRegion('details').show(wardLeaderView);
+          $(window).scrollTop(0);
+          e.preventDefault();
+     }
 });
 
 var TopLeadersView = Backbone.Marionette.CompositeView.extend({
      template: '#tmpl-top-leaders',
      childView: TopLeadersItemView,
-     childViewContainer: '#leaders'
+     childViewContainer: '#leaders',
+     initialize: function() {
+          _.bindAll(this, 'onFilter');
+     },
+     events: {
+          'click [data-filter]': 'onFilter'
+     },
+     onFilter: function(e) {
+          // Resort collection
+          var filter = $(e.currentTarget).data('filter');
+          this.collection.comparator = comparators[filter];
+          this.collection.sort();
+          
+          // Change active button
+          var button = this.$('[data-filter="'+filter+'"]');
+          button.parent().siblings().children('a').removeClass('active');
+          button.addClass('active');
+          e.preventDefault();
+     }
+});
+
+var WardLeaderView = Backbone.Marionette.ItemView.extend({
+     template: '#tmpl-details'
 });
 
 var getOrdinal = function(n) {
      var s=["th","st","nd","rd"],
           v=n%100;
      return n+(s[(v-20)%10]||s[v]||s[0]);
-}
+};
 
 var wardLeaders = new WardLeaders();
 wardLeaders.fetch({
      success: function(collection) {
-          console.log(collection.toJSON());
           var topLeadersView = new TopLeadersView({collection: wardLeaders});
-          layout.getRegion('main').show(topLeadersView);
+          layout.getRegion('top-leaders').show(topLeadersView);
      }
 });
