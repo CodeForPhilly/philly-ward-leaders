acc73e47a6067d2ba4af0d8772cd9cf72a313b7b
timwis <tim@timwis.com>
2016-05-02 11:33:23 -0400

    map D and R to party names in commitee persons dataset

--- src/collections/committee-persons.js
+++ src/collections/committee-persons.js
@@ -7,11 +7,15 @@ module.exports = Backbone.Collection.extend({
           if(options.ward) this.ward = options.ward;
           if(options.party) this.party = options.party;
      },
+     partyMap: {
+       D: 'Democrat',
+       R: 'Republican'
+     },
      url: function() {
           var url = 'https://data.phila.gov/resource/hezk-qta3.json',
             params = {};
           if(this.ward) params.ward = ('00' + this.ward).slice(-2); // pad left
-          if(this.party) params.party = this.party;
+          if(this.party) params.party = this.partyMap[this.party] || '';
           return url + '?' + $.param(params);
      },
      comparator: function(row) { return +row.get('division'); }
