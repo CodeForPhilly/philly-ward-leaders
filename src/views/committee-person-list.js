6ddfcff7745ec3abd25059ffd311da20bd93537f
timwis <tim@timwis.com>
2016-12-04 11:58:51 -0500

    delete v1 files

--- src/views/committee-person-list.js
+++
@@ -1,9 +0,0 @@
-var Backbone = require('backbone'),
-  Marionette = require('backbone.marionette'),
-  CommitteePersonItemView = require('../views/committee-person-item');
-
-module.exports = Backbone.Marionette.CollectionView.extend({
-  childView: CommitteePersonItemView,
-  tagName: 'ul',
-  className: 'small-block-grid-1 medium-block-grid-2 large-block-grid-3'
-});
