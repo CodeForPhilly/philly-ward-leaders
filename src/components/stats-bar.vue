e88cfccf2e9bb6e16bd9115d2494aac1817def67
timwis <tim@timwis.com>
2017-09-05 06:48:20 -0400

    Add remaining stats to ward leader page, move stats bar to component

---
+++ src/components/stats-bar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="level">
+    <div class="level-item has-text-centered">
+      <div>
+        <p class="heading">Voters ({{ party }})</p>
+        <p class="title">{{ partyRegistered }}</p>
+      </div>
+    </div>
+    <div class="level-item has-text-centered">
+      <div>
+        <p class="heading">Turnout ({{ party }})</p>
+        <p class="title">{{ partyTurnout }}</p>
+      </div>
+    </div>
+    <div class="level-item has-text-centered">
+      <div>
+        <p class="heading">Divisions</p>
+        <p class="title">{{ divisions }}</p>
+      </div>
+    </div>
+    <div class="level-item has-text-centered">
+      <div>
+        <p class="heading">Vacancies</p>
+        <p class="title">{{ vacancies }}</p>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: [
+    'party',
+    'party-registered',
+    'party-turnout',
+    'divisions',
+    'vacancies'
+  ]
+}
+</script>
--- src/routes/ward-leader.vue
+++ src/routes/ward-leader.vue
@@ -13,38 +13,100 @@
       </div>
     </section>
     <section class="section" v-if="leader">
-      <nav class="level">
-        <div class="level-item has-text-centered">
-          <div>
-            <p class="heading">Voters ({{ leader.Party }})</p>
-            <p class="title">{{ leader['Party Registered'] }}</p>
-          </div>
+      <stats-bar
+        :party="leader.Party"
+        :party-registered="leader['Party Registered']"
+        :party-turnout="leader['Party Turnout']"
+        :divisions="leader.Divisions"
+        :vacancies="vacancies"
+      ></stats-bar>
+    </section>
+    <section class="section" v-if="leader">
+      <div class="columns">
+        <div class="column">
+          <figure class="image is-square" v-if="leader.Photo">
+            <img :src="leader.Photo">
+          </figure>
         </div>
-        <div class="level-item has-text-centered">
-          <div>
-            <p class="heading">Turnout ({{ leader.Party }})</p>
-            <p class="title">{{ leader['Party Turnout'] }}</p>
-          </div>
+        <div class="column">
+          <dl>
+            <dt>Registered voters</dt>
+            <dd>
+              {{ leader['Party Registered'] }}
+              {{ partyPlural }} of
+              {{ leader['Total Registered'] }}
+              ({{ registeredPercent }}%)
+            </dd>
+
+            <dt>Turnout (2015 Primary)</dt>
+            <dd>
+              {{ leader['Party Turnout'] }}
+              {{ partyPlural }}
+              ({{ partyTurnoutPercent }}%)
+              <br>
+              {{ leader['Total Turnout'] }}
+              total
+              ({{ totalTurnoutPercent }}%)
+            </dd>
+
+            <dt>Divisions</dt>
+            <dd>{{ leader.Divisions }}</dd>
+
+            <dt>Committee Persons</dt>
+            <dd>
+              {{ leader['Committee People'] }}
+              ({{ vacancies }} vacancies)
+            </dd>
+          </dl>
         </div>
-        <div class="level-item has-text-centered">
-          <div>
-            <p class="heading">Divisions</p>
-            <p class="title">{{ leader.Divisions }}</p>
-          </div>
+        <div class="column">
+          <dl>
+            <dt>Address</dt>
+            <dd>{{ leader.Address }}</dd>
+
+            <dt>Phone</dt>
+            <dd>{{ leader.Phone }}</dd>
+
+            <dt>Age</dt>
+            <dd>{{ leader.Age }}</dd>
+
+            <dt>Gender</dt>
+            <dd>{{ leader.Gender }}</dd>
+
+            <dt>Occupation</dt>
+            <dd>{{ leader.Occupation }}</dd>
+          </dl>
         </div>
-        <div class="level-item has-text-centered">
-          <div>
-            <p class="heading">Vacancies</p>
-            <p class="title">{{ vacancies }}</p>
-          </div>
+        <div class="column">
+          <dl>
+            <dt>Email</dt>
+            <dd>{{ leader.Email }}</dd>
+
+            <dt>Social Media</dt>
+            <dd>
+              <ul>
+                <li v-if="leader.LinkedIn">
+                  <a :href="leader.LinkedIn">LinkedIn</a>
+                </li>
+                <li v-if="leader.Facebook">
+                  <a :href="leader.Facebook">Facebook</a>
+                </li>
+                <li v-if="leader.Twitter">
+                  <a :href="leader.Twitter">Twitter</a>
+                </li>
+              </ul>
+            </dd>
+          </dl>
         </div>
-      </nav>
+      </div>
     </section>
   </div>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex'
+
+import StatsBar from '../components/stats-bar.vue'
 
 export default {
   computed: {
@@ -56,9 +118,36 @@ export default {
         })
       }
     }),
+    partyPlural () {
+      return this.leader.Party === 'D' ? 'democrats' : 'republicans'
+    },
+    registeredPercent () {
+      const partyRegistered = this.leader['Party Registered']
+      const totalRegistered = this.leader['Total Registered']
+      return Math.round(partyRegistered / totalRegistered * 100)
+    },
+    partyTurnoutPercent () {
+      const partyTurnout = this.leader['Party Turnout']
+      const partyRegistered = this.leader['Party Registered']
+      return Math.round(partyTurnout / partyRegistered * 100)
+    },
+    totalTurnoutPercent () {
+      const totalTurnout = this.leader['Total Turnout']
+      const totalRegistered = this.leader['Total Registered']
+      return Math.round(totalTurnout / totalRegistered * 100)
+    },
     vacancies () {
       return this.leader.Divisions * 2 - this.leader['Committee People']
     }
+  },
+  components: {
+    'stats-bar': StatsBar
   }
 }
 </script>
+
+<style>
+dt {
+  font-weight: bold;
+}
+</style>
