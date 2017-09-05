a7eb879e4821e0d1e2a97170bbf467683bc31bae
timwis <tim@timwis.com>
2017-09-05 06:18:03 -0400

    Add splash page

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Splash from '../routes/splash.vue'
 import WardLeaderList from '../routes/ward-leader-list.vue'
 import WardLeader from '../routes/ward-leader.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', component: WardLeaderList },
+  { path: '/', component: Splash },
+  { path: '/leaders', component: WardLeaderList },
   { path: '/:ward/:party', component: WardLeader }
 ]
 
