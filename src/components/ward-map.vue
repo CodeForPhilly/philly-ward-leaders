8b8724086d0a2d3583de21ef6026c7b0d88a2aa2
timwis <tim@timwis.com>
2017-09-06 07:25:28 -0400

    Add static map to ward leader page

---
+++ src/components/ward-map.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-map style="height: 350px" :zoom="zoom" :center="center" :options="mapOpts">
+    <v-tilelayer :url="url" :attribution="attribution" :params="tileLayerOpts"></v-tilelayer>
+  </v-map>
+</template>
+
+<script>
+import { Map, TileLayer } from 'vue2-leaflet'
+
+export default {
+  name: 'ward-map',
+  components: {
+    'v-map': Map,
+    'v-tilelayer': TileLayer
+  },
+  data () {
+    return {
+      zoom: 16,
+      center: [39.9523893, -75.1636291],
+      mapOpts: {
+        scrollWheelZoom: false
+      },
+      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      url: 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}',
+      tileLayerOpts: {
+        subdomains: 'abcd',
+        minZoom: 0,
+        maxZoom: 20,
+        ext: 'png'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~leaflet/dist/leaflet.css";
+</style>
--- src/routes/ward-leader.vue
+++ src/routes/ward-leader.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <section class="hero is-info" v-if="leader">
+    <section class="hero is-info">
       <div class="hero-body">
         <div class="container">
           <h1 class="title">
@@ -12,7 +12,7 @@
         </div>
       </div>
     </section>
-    <section class="section" v-if="leader">
+    <section class="section stats-bar">
       <stats-bar
         :party="leader.party"
         :party-registered="leader.partyRegistered"
@@ -21,94 +21,101 @@
         :vacancies="vacancies"
       ></stats-bar>
     </section>
-    <section class="section" v-if="leader">
-      <div class="columns">
-        <div class="column">
-          <figure class="image" v-if="leader.photo">
-            <img :src="leader.photo">
-          </figure>
-        </div>
-        <div class="column">
-          <dl>
-            <dt>Registered voters</dt>
-            <dd>
-              {{ leader.partyRegistered }}
-              {{ partyPlural }} of
-              {{ leader.totalRegistered }}
-              ({{ registeredPercent }}%)
-            </dd>
+    <section class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <figure class="image" v-if="leader.photo">
+              <img :src="leader.photo">
+            </figure>
+          </div>
+          <div class="column">
+            <dl>
+              <dt>Registered voters</dt>
+              <dd>
+                {{ leader.partyRegistered }}
+                {{ partyPlural }} of
+                {{ leader.totalRegistered }}
+                ({{ registeredPercent }}%)
+              </dd>
 
-            <dt>Turnout (2015 Primary)</dt>
-            <dd>
-              {{ leader.partyTurnout }}
-              {{ partyPlural }}
-              ({{ partyTurnoutPercent }}%)
-              <br>
-              {{ leader.totalTurnout }}
-              total
-              ({{ totalTurnoutPercent }}%)
-            </dd>
+              <dt>Turnout (2015 Primary)</dt>
+              <dd>
+                {{ leader.partyTurnout }}
+                {{ partyPlural }}
+                ({{ partyTurnoutPercent }}%)
+                <br>
+                {{ leader.totalTurnout }}
+                total
+                ({{ totalTurnoutPercent }}%)
+              </dd>
 
-            <dt>Divisions</dt>
-            <dd>{{ leader.divisions }}</dd>
+              <dt>Divisions</dt>
+              <dd>{{ leader.divisions }}</dd>
 
-            <dt>Committee Persons</dt>
-            <dd>
-              {{ leader.committeePersons }}
-              ({{ vacancies }} vacancies)
-            </dd>
-          </dl>
-        </div>
-        <div class="column">
-          <dl>
-            <dt>Address</dt>
-            <dd>{{ leader.address }}</dd>
+              <dt>Committee Persons</dt>
+              <dd>
+                {{ leader.committeePersons }}
+                ({{ vacancies }} vacancies)
+              </dd>
+            </dl>
+          </div>
+          <div class="column">
+            <dl>
+              <dt>Address</dt>
+              <dd>{{ leader.address }}</dd>
 
-            <dt>Phone</dt>
-            <dd>{{ leader.phone }}</dd>
+              <dt>Phone</dt>
+              <dd>{{ leader.phone }}</dd>
 
-            <dt>Age</dt>
-            <dd>{{ leader.age }}</dd>
+              <dt>Age</dt>
+              <dd>{{ leader.age }}</dd>
 
-            <dt>Gender</dt>
-            <dd>{{ leader.gender }}</dd>
+              <dt>Gender</dt>
+              <dd>{{ leader.gender }}</dd>
 
-            <dt>Occupation</dt>
-            <dd>{{ leader.occupation }}</dd>
-          </dl>
-        </div>
-        <div class="column">
-          <dl>
-            <dt>Email</dt>
-            <dd>{{ leader.email }}</dd>
+              <dt>Occupation</dt>
+              <dd>{{ leader.occupation }}</dd>
+            </dl>
+          </div>
+          <div class="column">
+            <dl>
+              <dt>Email</dt>
+              <dd>{{ leader.email }}</dd>
 
-            <dt>Social Media</dt>
-            <dd>
-              <ul>
-                <li v-if="leader.linkedin">
-                  <a :href="leader.linkedin">LinkedIn</a>
-                </li>
-                <li v-if="leader.facebook">
-                  <a :href="leader.facebook">Facebook</a>
-                </li>
-                <li v-if="leader.twitter">
-                  <a :href="leader.twitter">Twitter</a>
-                </li>
-              </ul>
-            </dd>
-          </dl>
+              <dt>Social Media</dt>
+              <dd>
+                <ul>
+                  <li v-if="leader.linkedin">
+                    <a :href="leader.linkedin">LinkedIn</a>
+                  </li>
+                  <li v-if="leader.facebook">
+                    <a :href="leader.facebook">Facebook</a>
+                  </li>
+                  <li v-if="leader.twitter">
+                    <a :href="leader.twitter">Twitter</a>
+                  </li>
+                </ul>
+              </dd>
+            </dl>
+          </div>
         </div>
       </div>
     </section>
-    <section class="section" v-if="committeePersons">
-      <h2 class="title is-2">Committee Persons</h2>
-      <div class="columns is-multiline">
-        <committee-person
-          v-for="person in committeePersons"
-          :fullName="person.name"
-          :division="person.division"
-          :address="person.address"
-        ></committee-person>
+    <section class="section">
+      <ward-map :ward="leader.ward"></ward-map>
+    </section>
+    <section class="section">
+      <div class="container">
+        <h2 class="title is-2">Committee Persons</h2>
+        <div class="columns is-multiline">
+          <committee-person
+            v-for="person in committeePersons"
+            :fullName="person.name"
+            :division="person.division"
+            :address="person.address"
+          ></committee-person>
+        </div>
       </div>
     </section>
   </div>
@@ -119,6 +126,7 @@ import { mapState, mapActions } from 'vuex'
 
 import StatsBar from '../components/stats-bar.vue'
 import CommitteePerson from '../components/committee-person.vue'
+import WardMap from '../components/ward-map.vue'
 
 export default {
   computed: {
@@ -162,12 +170,16 @@ export default {
   },
   components: {
     'stats-bar': StatsBar,
-    'committee-person': CommitteePerson
+    'committee-person': CommitteePerson,
+    'ward-map': WardMap
   }
 }
 </script>
 
 <style>
+.stats-bar {
+  padding: 3rem 1.5rem 1.5rem 1.5rem;
+}
 dt {
   font-weight: bold;
 }
