3205249e6fd9e6d6d5848bcce9858c07557b9857
timwis <tim@timwis.com>
2017-09-15 05:54:07 -0400

    Remove committee persons with no ward/division

--- data-scripts/committee.py
+++ data-scripts/committee.py
@@ -1,6 +1,9 @@
 import petl as etl
 from slugify import slugify
 
+def has_ward_and_division(row):
+    return row['ward'] != '' and row['division'] != ''
+
 def clean_party(party):
     return 'democratic' if party == 'Democrat' else 'republican' if party == 'Republican' else None
 
@@ -14,6 +17,7 @@ def process_committee(filepath):
         .rename({'name': 'fullName'}) \
         .cut('ward', 'division', 'party', 'fullName',
              'address', 'zip') \
+        .select(has_ward_and_division) \
         .convert({'ward': int,
                   'division': int,
                   'fullName': 'title',
