38cda0c7ee78a31d829d62526d03a6c40df4ed4c
timwis <tim@timwis.com>
2017-09-19 05:59:31 -0400

    Router: add scroll behavior. Closes #123

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,16 @@ const routes = [
 
 const router = new VueRouter({
   mode: 'history',
-  routes
+  routes,
+  scrollBehavior
 })
+
+function scrollBehavior (to, from, savedPosition) {
+  if (savedPosition) {
+    return savedPosition
+  } else {
+    return { x: 0, y: 0 }
+  }
+}
 
 export default router
