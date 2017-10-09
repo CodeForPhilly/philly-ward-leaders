609aed0fd77eacdd39847c1fddf34ea93953bcb6
timwis <tim@timwis.com>
2017-10-09 08:31:24 -0400

    Add abstraction for client api calls. Increases readability of actions.js, which closes #138

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,154 +1,81 @@
-import axios from 'axios'
 import nanoid from 'nanoid'
-import { createClient } from 'contentful'
 
+import Api from '../api'
 import { CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN } from '../config'
 
-const client = createClient({
-  space: CONTENTFUL_SPACE_ID,
-  accessToken: CONTENTFUL_ACCESS_TOKEN
-})
+const api = new Api(CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN)
 
-export function NOTIFY ({ commit }, msg) {
+export function NOTIFY (ctx, msg) {
   const id = nanoid()
   const duration = 5000
   const notification = { id, msg }
-  commit('ADD_NOTIFICATION', notification)
-  window.setTimeout(() => commit('REMOVE_NOTIFICATION', id), duration)
+  ctx.commit('ADD_NOTIFICATION', notification)
+  window.setTimeout(() => ctx.commit('REMOVE_NOTIFICATION', id), duration)
 }
 
 export async function FETCH_LEADERS (ctx) {
   ctx.commit('BEGIN_REQUEST', 'FETCH_LEADERS')
-  const fields = [
-    'sys.id',
-    'fields.ward',
-    'fields.party',
-    'fields.fullName',
-    'fields.photoUrl',
-    'fields.registeredVotersParty',
-    'fields.turnoutParty',
-    'fields.divisionCount',
-    'fields.committeePersonCount'
-  ]
-  let response
   try {
-    const opts = {
-      content_type: 'wardLeader',
-      order: 'fields.ward',
-      select: fields.join(','),
-      limit: 1000
-    }
-    response = await client.getEntries(opts)
+    const leaders = await api.fetchLeaders()
+    ctx.commit('FETCH_LEADERS_SUCCESS', leaders)
   } catch (err) {
     ctx.dispatch('NOTIFY', `Failed to retrieve ward leaders`)
-    ctx.commit('END_REQUEST', 'FETCH_LEADERS')
-    return
   }
-  const leaders = response.items.map(getFieldsAndId)
-  ctx.commit('FETCH_LEADERS_SUCCESS', leaders)
   ctx.commit('END_REQUEST', 'FETCH_LEADERS')
 }
 
 export async function FETCH_LEADER (ctx, { ward, party }) {
-  ctx.commit('BEGIN_REQUEST', 'FETCH_LEADER')
   ctx.commit('RESET_LEADER')
-  let response
+  ctx.commit('BEGIN_REQUEST', 'FETCH_LEADER')
   try {
-    response = await client.getEntries({
-      content_type: 'wardLeader',
-      'fields.ward': ward,
-      'fields.party': party
-    })
+    const leader = await api.fetchLeader(ward, party)
+    ctx.commit('FETCH_LEADER_SUCCESS', leader)
   } catch (err) {
     ctx.dispatch('NOTIFY', `Failed to get information about the ward leader`)
-    ctx.commit('END_REQUEST', 'FETCH_LEADER')
-    return
-  }
-
-  if (response.items.length) {
-    const leader = getFieldsAndId(response.items[0])
-    ctx.commit('FETCH_LEADER_SUCCESS', leader)
-  } else {
-    ctx.dispatch('NOTIFY', `Ward leader was not found`)
   }
   ctx.commit('END_REQUEST', 'FETCH_LEADER')
 }
 
 export async function FETCH_COMMITTEE_PERSONS (ctx, { ward, party }) {
   ctx.commit('BEGIN_REQUEST', 'FETCH_COMMITTEE_PERSONS')
-  let response
   try {
-    response = await client.getEntries({
-      content_type: 'committeePerson',
-      order: 'fields.division',
-      'fields.ward': ward,
-      'fields.party': party
-    })
+    const committeePersons = await api.fetchCommitteePersons(ward, party)
+    ctx.commit('FETCH_COMMITTEE_PERSONS_SUCCESS', committeePersons)
   } catch (err) {
     ctx.dispatch('NOTIFY', `Failed to get list of committee persons`)
-    ctx.commit('END_REQUEST', 'FETCH_COMMITTEE_PERSONS')
-    return
   }
-  const committeePersons = response.items.map(getFieldsAndId)
-  ctx.commit('FETCH_COMMITTEE_PERSONS_SUCCESS', committeePersons)
   ctx.commit('END_REQUEST', 'FETCH_COMMITTEE_PERSONS')
 }
 
 export async function FETCH_WARD_BOUNDARIES (ctx, ward) {
   ctx.commit('BEGIN_REQUEST', 'FETCH_WARD_BOUNDARIES')
-  let response
   try {
-    const url = `/data/ward-boundaries/${ward}.geojson`
-    response = await axios.get(url)
+    const wardBoundaries = await api.fetchWardBoundaries(ward)
+    ctx.commit('FETCH_WARD_BOUNDARIES_SUCCESS', wardBoundaries)
   } catch (err) {
     ctx.commit('END_REQUEST', 'FETCH_WARD_BOUNDARIES')
-    return
   }
-  ctx.commit('FETCH_WARD_BOUNDARIES_SUCCESS', response.data)
   ctx.commit('END_REQUEST', 'FETCH_WARD_BOUNDARIES')
 }
 
 export async function FETCH_CITYWIDE_BOUNDARIES (ctx) {
   ctx.commit('BEGIN_REQUEST', 'FETCH_CITYWIDE_BOUNDARIES')
-  let response
   try {
-    const url = `/data/citywide-boundaries.geojson`
-    response = await axios.get(url)
+    const citywideBoundaries = await api.fetchCitywideBoundaries()
+    ctx.commit('CITYWIDE_BOUNDARIES_SUCCESS', citywideBoundaries)
   } catch (err) {
     ctx.dispatch('NOTIFY', `Failed to retrieve ward boundaries`)
-    ctx.commit('END_REQUEST', 'FETCH_CITYWIDE_BOUNDARIES')
-    return
   }
-  ctx.commit('FETCH_CITYWIDE_BOUNDARIES_SUCCESS', response.data)
   ctx.commit('END_REQUEST', 'FETCH_CITYWIDE_BOUNDARIES')
 }
 
 export async function FETCH_CONTENT_PAGE (ctx, slug) {
   ctx.commit('BEGIN_REQUEST', 'FETCH_CONTENT_PAGE')
-  let response
   try {
-    response = await client.getEntries({
-      content_type: 'page',
-      'fields.slug': slug
-    })
+    const contentPage = api.fetchContentPage(slug)
+    ctx.commit('FETCH_CONTENT_PAGE_SUCCESS', contentPage)
   } catch (err) {
     ctx.dispatch('NOTIFY', `Failed to retrieve content`)
-    ctx.commit('END_REQUEST', 'FETCH_CONTENT_PAGE')
-    return
-  }
-
-  if (response.items.length) {
-    const contentPage = getFieldsAndId(response.items[0])
-    ctx.commit('FETCH_CONTENT_PAGE_SUCCESS', contentPage)
-  } else {
-    ctx.dispatch('NOTIFY', `Page not found`)
   }
   ctx.commit('END_REQUEST', 'FETCH_CONTENT_PAGE')
-}
-
-function getFieldsAndId (item) {
-  return {
-    ...item.fields,
-    id: item.sys.id
-  }
 }
