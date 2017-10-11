d5ee3e69a52be20f12e802568f15ff419a53e369
timwis <tim@timwis.com>
2017-10-11 07:00:03 -0400

    Add remaining actions tests. Closes #155

--- tests/store/actions.spec.js
+++ tests/store/actions.spec.js
@@ -1,9 +1,5 @@
 import 'babel-polyfill'
 
-// import * as actions from '../../src/store/actions'
-
-// jest.mock('../../src/api')
-
 describe('Actions', () => {
   beforeEach(() => {
     jest.resetModules()
@@ -34,12 +30,8 @@ describe('Actions', () => {
 
   describe('Fetch leaders', () => {
     it('Successful action calls begin, success, and end', () => {
-      const payload = ['yay']
-      jest.doMock('../../src/api', () => class {
-        fetchLeaders () {
-          return Promise.resolve(payload)
-        }
-      })
+      const fakeLeaders = ['yay']
+      mockApiSuccess('fetchLeaders', fakeLeaders)
       const { FETCH_LEADERS } = require('../../src/store/actions')
 
       const commit = jest.fn()
@@ -47,17 +39,13 @@ describe('Actions', () => {
 
       process.nextTick(() => {
         expect(commit).toBeCalledWith('BEGIN_REQUEST', 'FETCH_LEADERS')
-        expect(commit).toBeCalledWith('FETCH_LEADERS_SUCCESS', payload)
+        expect(commit).toBeCalledWith('FETCH_LEADERS_SUCCESS', fakeLeaders)
         expect(commit).toBeCalledWith('END_REQUEST', 'FETCH_LEADERS')
       })
     })
 
     it('Failed action calls begin, notify, and end', () => {
-      jest.doMock('../../src/api', () => class {
-        fetchLeaders () {
-          return Promise.reject(new Error())
-        }
-      })
+      mockApiFailure('fetchLeaders')
       const { FETCH_LEADERS } = require('../../src/store/actions')
 
       const commit = jest.fn()
@@ -73,29 +61,182 @@ describe('Actions', () => {
   })
 
   describe('Fetch leader', () => {
-    it('Puts the ward and party in the query url')
-    it('Considers an empty array like a 404')
-    it('Calls fetch_leader_success with the appropriate data structure')
+    it('Calls reset, begin, success, and end', () => {
+      const fakeLeader = { name: 'John Doe' }
+      mockApiSuccess('fetchLeader', fakeLeader)
+      const { FETCH_LEADER } = require('../../src/store/actions')
+
+      const commit = jest.fn()
+      const payload = { ward: 1, party: 'democratic' }
+      FETCH_LEADER({ commit }, payload)
+
+      process.nextTick(() => {
+        expect(commit).toBeCalledWith('RESET_LEADER')
+        expect(commit).toBeCalledWith('BEGIN_REQUEST', 'FETCH_LEADER')
+        expect(commit).toBeCalledWith('FETCH_LEADER_SUCCESS', fakeLeader)
+        expect(commit).toBeCalledWith('END_REQUEST', 'FETCH_LEADER')
+      })
+    })
+
+    it('Calls notify and end on failure', () => {
+      mockApiFailure('fetchLeader')
+      const { FETCH_LEADER } = require('../../src/store/actions')
+
+      const commit = jest.fn()
+      const dispatch = jest.fn()
+      const payload = { ward: 1, party: 'democratic' }
+      FETCH_LEADER({ commit, dispatch }, payload)
+
+      process.nextTick(() => {
+        expect(dispatch).toBeCalledWith('NOTIFY', expect.any(String))
+        expect(commit).toBeCalledWith('END_REQUEST', 'FETCH_LEADER')
+      })
+    })
   })
 
   describe('Fetch committee persons', () => {
-    it('Puts the ward and aprty in the query url')
-    it('Saves the result with the proper data structure')
+    it('Calls begin, success, and end', () => {
+      const fakeResponse = [{ name: 'John Doe' }]
+      mockApiSuccess('fetchCommitteePersons', fakeResponse)
+      const { FETCH_COMMITTEE_PERSONS } = require('../../src/store/actions')
+
+      const commit = jest.fn()
+      const payload = { ward: 1, party: 'democratic' }
+      FETCH_COMMITTEE_PERSONS({ commit }, payload)
+
+      process.nextTick(() => {
+        expect(commit).toBeCalledWith('BEGIN_REQUEST', 'FETCH_COMMITTEE_PERSONS')
+        expect(commit).toBeCalledWith('FETCH_COMMITTEE_PERSONS_SUCCESS', fakeResponse)
+        expect(commit).toBeCalledWith('END_REQUEST', 'FETCH_COMMITTEE_PERSONS')
+      })
+    })
+
+    it('Calls notify and end on failure', () => {
+      mockApiFailure('fetchCommitteePersons')
+      const { FETCH_COMMITTEE_PERSONS } = require('../../src/store/actions')
+
+      const commit = jest.fn()
+      const dispatch = jest.fn()
+      const payload = { ward: 1, party: 'democratic' }
+      FETCH_COMMITTEE_PERSONS({ commit, dispatch }, payload)
+
+      process.nextTick(() => {
+        expect(dispatch).toBeCalledWith('NOTIFY', expect.any(String))
+        expect(commit).toBeCalledWith('END_REQUEST', 'FETCH_COMMITTEE_PERSONS')
+      })
+    })
   })
 
   describe('Fetch ward boundaries', () => {
-    it('Constructs the appropriate url')
-    it('Calls end_request on failure')
+    it('Calls begin, success, and end', () => {
+      const fakeResponse = [{ type: 'Feature' }]
+      mockApiSuccess('fetchWardBoundaries', fakeResponse)
+      const { FETCH_WARD_BOUNDARIES } = require('../../src/store/actions')
+
+      const commit = jest.fn()
+      const payload = 1
+      FETCH_WARD_BOUNDARIES({ commit }, payload)
+
+      process.nextTick(() => {
+        expect(commit).toBeCalledWith('BEGIN_REQUEST', 'FETCH_WARD_BOUNDARIES')
+        expect(commit).toBeCalledWith('FETCH_WARD_BOUNDARIES_SUCCESS', fakeResponse)
+        expect(commit).toBeCalledWith('END_REQUEST', 'FETCH_WARD_BOUNDARIES')
+      })
+    })
+
+    it('Calls end on failure', () => {
+      mockApiFailure('fetchWardBoundaries')
+      const { FETCH_WARD_BOUNDARIES } = require('../../src/store/actions')
+
+      const commit = jest.fn()
+      const dispatch = jest.fn()
+      const payload = 1
+      FETCH_WARD_BOUNDARIES({ commit, dispatch }, payload)
+
+      process.nextTick(() => {
+        expect(commit).toBeCalledWith('END_REQUEST', 'FETCH_WARD_BOUNDARIES')
+      })
+    })
   })
 
   describe('Fetch citywide boundaries', () => {
-    // Same as ward boundaries
-    it('Calls notify on failure')
+    it('Calls begin, success, and end', () => {
+      const fakeResponse = [{ type: 'Feature' }]
+      mockApiSuccess('fetchCitywideBoundaries', fakeResponse)
+      const { FETCH_CITYWIDE_BOUNDARIES } = require('../../src/store/actions')
+
+      const commit = jest.fn()
+      FETCH_CITYWIDE_BOUNDARIES({ commit })
+
+      process.nextTick(() => {
+        expect(commit).toBeCalledWith('BEGIN_REQUEST', 'FETCH_CITYWIDE_BOUNDARIES')
+        expect(commit).toBeCalledWith('FETCH_CITYWIDE_BOUNDARIES_SUCCESS', fakeResponse)
+        expect(commit).toBeCalledWith('END_REQUEST', 'FETCH_CITYWIDE_BOUNDARIES')
+      })
+    })
+
+    it('Calls notify and end on failure', () => {
+      mockApiFailure('fetchCitywideBoundaries')
+      const { FETCH_CITYWIDE_BOUNDARIES } = require('../../src/store/actions')
+
+      const commit = jest.fn()
+      const dispatch = jest.fn()
+      FETCH_CITYWIDE_BOUNDARIES({ commit, dispatch })
+
+      process.nextTick(() => {
+        expect(dispatch).toBeCalledWith('NOTIFY', expect.any(String))
+        expect(commit).toBeCalledWith('END_REQUEST', 'FETCH_CITYWIDE_BOUNDARIES')
+      })
+    })
   })
 
   describe('Fetch content page', () => {
-    it('Puts the slug in the query url')
-    it('Considers an empty array like a 404')
-    it('Saves the first item')
+    it('Calls begin, success, and end', () => {
+      const fakeResponse = { content: 'Foo' }
+      mockApiSuccess('fetchContentPage', fakeResponse)
+      const { FETCH_CONTENT_PAGE } = require('../../src/store/actions')
+
+      const commit = jest.fn()
+      const payload = 'about'
+      FETCH_CONTENT_PAGE({ commit }, payload)
+
+      process.nextTick(() => {
+        expect(commit).toBeCalledWith('BEGIN_REQUEST', 'FETCH_CONTENT_PAGE')
+        expect(commit).toBeCalledWith('FETCH_CONTENT_PAGE_SUCCESS', fakeResponse)
+        expect(commit).toBeCalledWith('END_REQUEST', 'FETCH_CONTENT_PAGE')
+      })
+    })
+
+    it('Calls notify and end on failure', () => {
+      mockApiFailure('fetchContentPage')
+      const { FETCH_CONTENT_PAGE } = require('../../src/store/actions')
+
+      const commit = jest.fn()
+      const dispatch = jest.fn()
+      const payload = 'slug'
+      FETCH_CONTENT_PAGE({ commit, dispatch }, payload)
+
+      process.nextTick(() => {
+        expect(dispatch).toBeCalledWith('NOTIFY', expect.any(String))
+        expect(commit).toBeCalledWith('END_REQUEST', 'FETCH_CONTENT_PAGE')
+      })
+    })
+
   })
 })
+
+function mockApiSuccess (method, response) {
+  jest.doMock('../../src/api', () => class {
+    [method] () {
+      return Promise.resolve(response)
+    }
+  })
+}
+
+function mockApiFailure (method) {
+  jest.doMock('../../src/api', () => class {
+    [method] () {
+      return Promise.reject(new Error())
+    }
+  })
+}
