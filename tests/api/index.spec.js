3cbd3118a360298ae6b5fd5bed799a1084ea6df7
timwis <tim@timwis.com>
2017-10-14 12:01:51 -0400

    Add api tests for committee persons and content page

--- tests/api/index.spec.js
+++ tests/api/index.spec.js
@@ -61,6 +61,60 @@ describe('Api', () => {
       await expect(api.fetchLeader()).rejects.toBeDefined()
     })
   })
+
+  describe('Fetch committee persons', () => {
+    it('Simplifies response data structure', async () => {
+      const fakeResponse = require('./__fixtures__/committeePersons.json')
+      mockClientSuccess(fakeResponse)
+      const Api = require('../../src/api').default
+      const api = new Api()
+      const response = await api.fetchCommitteePersons()
+
+      expect(response.length).toBeGreaterThan(0)
+      const item = response[0]
+      expect(item).not.toHaveProperty('fields')
+      expect(item).not.toHaveProperty('sys')
+      expect(item).toHaveProperty('id')
+      expect(item).toHaveProperty('fullName')
+    })
+
+    it('Rejects if request fails', async () => {
+      mockClientFailure()
+      const Api = require('../../src/api').default
+      const api = new Api()
+      await expect(api.fetchCommitteePersons()).rejects.toBeDefined()
+    })
+  })
+
+  describe('Fetch content page', () => {
+    it('Simplifies response data structure', async () => {
+      const fakeResponse = require('./__fixtures__/contentPage.json')
+      mockClientSuccess(fakeResponse)
+      const Api = require('../../src/api').default
+      const api = new Api()
+      const response = await api.fetchContentPage()
+
+      expect(response).not.toHaveProperty('fields')
+      expect(response).not.toHaveProperty('sys')
+      expect(response).toHaveProperty('id')
+      expect(response).toHaveProperty('content')
+    })
+
+    it('Rejects if page not found', async () => {
+      const fakeResponse = require('./__fixtures__/contentPage-notFound.json')
+      mockClientSuccess(fakeResponse)
+      const Api = require('../../src/api').default
+      const api = new Api()
+      await expect(api.fetchContentPage()).rejects.toBeDefined()
+    })
+
+    it('Rejects if request fails', async () => {
+      mockClientFailure()
+      const Api = require('../../src/api').default
+      const api = new Api()
+      await expect(api.fetchContentPage()).rejects.toBeDefined()
+    })
+  })
 })
 
 function mockClientSuccess (response) {
