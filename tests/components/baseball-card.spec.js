524db024f1083554c3f80764dc8d1daa6ee593fb
timwis <tim@timwis.com>
2018-01-31 08:22:57 -0500

    Add tests for subwards. Supports #179

--- tests/components/baseball-card.spec.js
+++ tests/components/baseball-card.spec.js
@@ -26,4 +26,25 @@ describe('Baseball card', () => {
       expect(str).toMatchSnapshot()
     })
   })
+
+  it('Includes subward in URL', () => {
+    const propsData = {
+      name: 'John Doe',
+      ward: 1,
+      subWard: 'A',
+      party: 'democratic',
+      photoUrl: 'https://example.com/image.png',
+      registeredVotersParty: 1000,
+      turnoutParty: 400,
+      turnoutTotal: 600,
+      divisionCount: 30,
+      committeePersonCount: 25
+    }
+    const wrapper = mount(BaseballCard, {
+      propsData,
+      stubs: ['router-link']
+    })
+    const url = wrapper.vm.url
+    expect(url).toBe('/leaders/democratic/1A/john-doe')
+  })
 })
---
+++ tests/views/ward-leader.spec.js
@@ -0,0 +1,69 @@
+import { mount } from 'vue-test-utils'
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+import WardLeader from '../../src/views/ward-leader.vue'
+
+Vue.use(Vuex)
+
+describe('Ward leader', () => {
+  it('Fetches leader based on ward and party', () => {
+    const FETCH_LEADER = jest.fn()
+    const FETCH_COMMITTEE_PERSONS = jest.fn()
+    const FETCH_WARD_BOUNDARIES = jest.fn()
+    const $store = new Vuex.Store({
+      state: {
+        currentLeader: {
+          leader: {},
+          committeePersons: [],
+          wardBoundaries: {}
+        }
+      },
+      actions: {
+        FETCH_LEADER,
+        FETCH_COMMITTEE_PERSONS,
+        FETCH_WARD_BOUNDARIES
+      }
+    })
+    const propsData = { ward: '66', party: 'democratic' }
+    mount(WardLeader, {
+      mocks: { $store },
+      propsData
+    })
+
+    expect(FETCH_LEADER).toBeCalled()
+    expect(FETCH_LEADER.mock.calls[0][1].party).toBe('democratic')
+    expect(FETCH_LEADER.mock.calls[0][1].ward).toBe('66')
+    expect(FETCH_LEADER.mock.calls[0][1].subWard).toBe(undefined)
+  })
+
+  it('Fetches leader based on ward, party, and subward', () => {
+    const FETCH_LEADER = jest.fn()
+    const FETCH_COMMITTEE_PERSONS = jest.fn()
+    const FETCH_WARD_BOUNDARIES = jest.fn()
+    const $store = new Vuex.Store({
+      state: {
+        currentLeader: {
+          leader: {},
+          committeePersons: [],
+          wardBoundaries: {}
+        }
+      },
+      actions: {
+        FETCH_LEADER,
+        FETCH_COMMITTEE_PERSONS,
+        FETCH_WARD_BOUNDARIES
+      }
+    })
+    const propsData = { ward: '66A', party: 'democratic' }
+    mount(WardLeader, {
+      mocks: { $store },
+      propsData
+    })
+
+    expect(FETCH_LEADER).toBeCalled()
+    expect(FETCH_LEADER.mock.calls[0][1].party).toBe('democratic')
+    expect(FETCH_LEADER.mock.calls[0][1].ward).toBe('66')
+    expect(FETCH_LEADER.mock.calls[0][1].subWard).toBe('A')
+  })
+})
