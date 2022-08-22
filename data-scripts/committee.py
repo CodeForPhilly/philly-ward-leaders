89b8fff41d865a07198b301c39a6980d13c3447e
Ollie <ojimba01@gmail.com>
2022-08-21 21:26:07 -0400

    Commitee.py update (#240)
    Update committee.py
    
    Deleted expand_zip function, edited process_commitee function to be up to date with the csv headers, and edited create_division function to adjust to the new ward_div format

--- data-scripts/committee.py
+++ data-scripts/committee.py
@@ -1,32 +1,27 @@
 import petl as etl
 
-def expand_zip(zip):
-    return '191' + zip
-
-
 def create_ward(row):
-    # Get characters before -, trim leading zeros, convert to int
     return int(row['ward_div'].split('-')[0].lstrip('0'))
 
 def create_division(row):
+    # Delete "DEM" or "REP" from the end of the name
     # Get characters after -, trim leading zeros, convert to int
-    return int(row['ward_div'].split('-')[1].lstrip('0'))
+    return int(row['ward_div'].split('-')[1].lstrip('0').strip('DEM').strip('REP'))
 
 def process_committee(filepath):
     table = etl.fromcsv(filepath) \
-        .rename({'PRECINCT': 'ward_div',
-                 'PARTY': 'party',
-                 'SELECTION': 'fullName',
-                 'STREET': 'address',
-                 'ZIP': 'zip'}) \
+        .rename({'Race': 'ward_div',
+                 'Party': 'party',
+                 'Name': 'fullName',
+                 'Add1': 'address',
+                 'Zip': 'zip'}) \
         .cut('ward_div', 'party', 'fullName',
              'address', 'zip') \
         .convert({'fullName': 'title',
                   'address': 'title'}) \
         .convert('party', 'lower') \
-        .convert('zip', expand_zip) \
         .addfield('ward', create_ward, index=0) \
         .addfield('division', create_division, index=1) \
         .cutout('ward_div')
-
+    
     return table
