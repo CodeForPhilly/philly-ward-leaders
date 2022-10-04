cdfe083569dff5a476690cfe570e8f5d9524ba71
pwolanin <pwolanin@49851.no-reply.drupal.org>
2022-10-04 18:44:16 -0400

    mess up spacing

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import router from './router'
+   import router from './router'
 import store from './store'
 
 new Vue({ // eslint-disable-line
   el: '#app',
-  render: h => h(App),
+     render: h => h(App),
   router,
   store
 })
