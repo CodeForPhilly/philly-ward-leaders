96b402da8e392b49c7163eab1a109f0dcaf56068
katedev21 <Kate.Moore@coravin.com>
2022-10-11 21:14:00 -0400

    remove / in index.html and webpack public path

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: [ 'babel-polyfill', './src/main.js' ],
   output: {
     path: path.resolve(__dirname, './public'),
-    publicPath: '/',
+    publicPath: '',
     filename: 'build.js'
   },
   module: {
