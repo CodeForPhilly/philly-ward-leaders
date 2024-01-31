ac6747db4f974feb6ec52af4122aefd277fa8f29
vvi230714 <vignesh.v.iyer@gmail.com>
2024-01-31 23:01:21 +0000

    Updated webpack vue loader config

--- webpack.config.js
+++ webpack.config.js
@@ -13,13 +13,31 @@ module.exports = {
       {
         test: /\.vue$/,
         loader: 'vue-loader',
-        options: {
-          loaders: {
-            'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
-          }
-        }
       },
+      {
+          test: /\.scss$/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+            'sass-loader'
+          ]
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              indentedSyntax: true,
+              sassOptions: {
+                indentedSyntax: true
+              }
+            }
+          }
+        ]
+    },
       {
         test: /\.js$/,
         loader: 'babel-loader',
