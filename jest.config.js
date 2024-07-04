df98caada69db599cb14f44f1ca0d8f9f183572d
vvi230714 <vignesh.v.iyer@gmail.com>
2024-07-04 22:26:33 +0000

    First working test

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    testPathIgnorePatterns: [
+      "/node_modules/",
+      "<rootDir>/tests/app.spec.js"
+    ],
+    testMatch: [
+      '**/tests/**/*.spec.[jt]s?(x)',
+      '**/__tests__/*.[jt]s?(x)'
+    ],
+    moduleFileExtensions: [
+      "js",
+      "json",
+      "vue"
+    ],
+    transform: {
+      ".*\\.(vue)$": "@vue/vue3-jest",
+      "^.+\\.js$": "babel-jest"
+    },
+    snapshotSerializers: [
+      "jest-serializer-vue"
+    ],
+    collectCoverageFrom: [
+      "src/**/*.{js,vue}"
+    ],
+    testEnvironment: 'jsdom',
+    testEnvironmentOptions: {
+      customExportConditions: ["node", "node-addons"],
+    //setupFiles: ['jest.setup.js']
+   }
+  }
