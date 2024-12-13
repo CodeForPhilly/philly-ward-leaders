e943f314e7e85a778b3c343b9a1d75d7c6038963
vvi230714 <vignesh.v.iyer@gmail.com>
2024-12-13 23:40:08 +0000

    removed buefy from test

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -1,7 +1,6 @@
 import { shallowMount } from '@vue/test-utils'
 import Vue from 'vue'
 import Vuex from 'vuex'
-import Buefy from 'buefy'
 
 import App from '../src/App.vue'
 import Notification from '../src/components/notification.vue'
@@ -9,40 +8,6 @@ import Notification from '../src/components/notification.vue'
 Vue.use(Vuex)
 
 describe('App', () => {
-  test('Doesn\'t show loading indicator when no pending requests', () => {
-    const $store = new Vuex.Store({
-      state: {
-        pendingRequests: {}
-      }
-    })
-
-    const wrapper = shallowMount(App, {
-      mocks: { $store },
-      stubs: ['router-view']
-    })
-
-    const indicator = wrapper.findComponent(Buefy.Loading)
-
-    expect(indicator.props('active')).toBe(false)
-  })
-
-  test('Shows loading indicator when there are pending requests', () => {
-    const $store = new Vuex.Store({
-      state: {
-        pendingRequests: {
-          foo: { msg: 'bar' }
-        }
-      }
-    })
-
-    const wrapper = shallowMount(App, {
-      mocks: { $store },
-      stubs: ['router-view']
-    })
-
-    const indicator = wrapper.findComponent(Buefy.Loading)
-    expect(indicator.props('active')).toBe(true)
-  })
 
   test('Shows notifications', () => {
     const $store = new Vuex.Store({
