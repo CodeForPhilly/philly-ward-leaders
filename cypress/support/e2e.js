98303a63c0c020495e2495a48dc1be952278e9b8
Peter Wolanin <pwolanin@users.noreply.github.com>
2026-06-23 20:14:08 -0400

    try to fix e2e

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,8 +1,8 @@
 import { mount } from 'cypress/vue';
 import  Api  from '../../src/api';
-import { CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN } from '../../src/config'
+import { CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN, CONTENTFUL_ENVIRONMENT } from '../../src/config'
 
-const api = new Api(CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN)
+const api = new Api(CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN, CONTENTFUL_ENVIRONMENT)
 
 Cypress.Commands.add('mount', mount);
 Cypress.Commands.add('fetchLeaders', () => cy.wrap(api.fetchLeaders()));
